908d774df29a8f7f68fc30f3031e93aa19658d0f
Nathen Drees <nadrees@gmail.com>
2013-05-31 22:18:17 -0500

    Solved problem 1

---
+++ 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = Enumerable.Range(0, 1000).Where(i => i % 3 == 0 || i % 5 == 0).Sum();
+            Console.WriteLine(answer);
+            Console.ReadKey();
+        }
+    }
+}
