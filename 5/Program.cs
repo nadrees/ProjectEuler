f6bb3d033f4b3096732a3371f060009d4291606a
Nathen Drees <nadrees@gmail.com>
2013-06-01 16:31:21 -0500

    Solved problem 5

---
+++ 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2520; true; i++)
+            {
+                if (IsEvenlyDivisible(i))
+                {
+                    Console.WriteLine(i);
+                    Console.ReadKey();
+                    break;
+                }
+            }
+        }
+
+        private static bool IsEvenlyDivisible(int num)
+        {
+            for (int i = 2; i <= 20; i++)
+            {
+                if (num % i != 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
