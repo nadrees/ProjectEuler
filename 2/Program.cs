034e9b28a3ac722539764ad5f25eecc534e1f108
Nathen Drees <nadrees@gmail.com>
2013-05-31 22:34:13 -0500

    Solved problem 2

---
+++ 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = FibonacciGenerator.Generate()
+                .TakeWhile((value) => value <= 4000000)
+                .Where(v => v % 2 == 0)
+                .Select(v => (int)v)
+                .Sum();
+
+            Console.WriteLine(answer);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lib/SimpleNumberGenerators.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    public class FibonacciGenerator
+    {
+        public static IEnumerable<uint> Generate()
+        {
+            uint i1 = 1;
+            uint i2 = 2;
+
+            yield return i1;
+            yield return i2;
+
+            while (true)
+            {
+                uint next = i1 + i2;
+                yield return next;
+
+                i1 = i2;
+                i2 = next;
+            }
+        }
+    }
+}
